e91a89f41b9d3c5d9b31ed8ffca52b7ee0058bbd
Alfredo <alfredomoreno@servtop.com.ar>
2025-01-23 11:19:05 -0300

    método en register corregido

--- app/routes/paciente_bp.py
+++ app/routes/paciente_bp.py
@@ -3,7 +3,7 @@ from ..controllers.paciente_controller import PacienteController
 
 paciente_bp = Blueprint('paciente_bp', __name__)
 
-paciente_bp.route('/register', methods=['GET','POST'])(PacienteController.register)
+paciente_bp.route('/register', methods=['POST'])(PacienteController.register)
 paciente_bp.route('/crear_turno', methods=['POST'])(PacienteController.crearTurno)
 paciente_bp.route('/cancelar_turno/<int:id_turno>', methods=['POST'])(PacienteController.cancelarTurno)
 paciente_bp.route('/datos_paciente', methods=['GET'])(PacienteController.getInfo)
